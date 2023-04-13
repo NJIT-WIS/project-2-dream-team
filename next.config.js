68bca13a16e35ced475d54dbdae4fd379f3c2840
Rajiv Kumar <rk864@njit.edu>
2023-04-12 22:18:03 -0400

    2nd attempt on next.config.js to remove i18n support

--- next.config.js
+++ next.config.js
@@ -1,29 +1,9 @@
-const nextConfig = {
-  i18n: {
-    locales: ["en", "fr", "es"],
-    defaultLocale: "en",
-  },
+module.exports = {
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
-    const localePaths = [];
-
-    for (const locale of nextConfig.i18n.locales) {
-      const localePathMap = {};
-
-      for (const [path, page] of Object.entries(defaultPathMap)) {
-        const localePath = `/${locale}${path === "/" ? "" : path}`;
-        localePathMap[localePath] = { ...page, query: { ...page.query, locale } };
-      }
-
-      localePaths.push(localePathMap);
-    }
-
-    const pathMap = Object.assign({}, ...localePaths);
-
-    return pathMap;
+    return defaultPathMap;
   },
 };
 
-module.exports = nextConfig;
