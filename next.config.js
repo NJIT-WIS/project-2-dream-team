7c8bf5dc7c2e90d94f1223fb746475d4c3a488b5
Rajiv Kumar <rk864@njit.edu>
2023-04-12 20:10:43 -0400

    after review, updated next.config.js which resolves issues with i18n configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
-/** @type {import('next').NextConfig} */
+const locales = ["en", "fr", "es"];
 
-const nextConfig = {
+module.exports = {
   i18n: {
-    locales: ["en"],
+    locales,
     defaultLocale: "en",
   },
-};
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    // Generate a mapping of paths to exported HTML pages for each locale
+    const pathMap = {};
 
-module.exports = nextConfig;
+    for (const locale of locales) {
+      for (const [path, page] of Object.entries(defaultPathMap)) {
+        // Add the locale prefix to the path for each locale
+        const localePath = `/${locale}${path === "/" ? "" : path}`;
+
+        // Add the page to the pathMap for the current locale
+        pathMap[localePath] = { ...page, query: { ...page.query, locale } };
+      }
+    }
+
+    return pathMap;
+  },
+};
