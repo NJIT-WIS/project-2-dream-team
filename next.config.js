cfb56f8766ac736e9845e120cc8ae54e0682b8eb
Rajiv Kumar <rk864@njit.edu>
2023-04-18 19:49:51 -0400

    changed in next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,36 +1,6 @@
-const withImages = require('next-images')
-const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
-
-module.exports = withPlugins([
-  withImages,
-  [optimizedImages, {
-    handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
-    optimizeImages: true,
-    optimizeImagesInDev: true,
-    mozjpeg: {
-      quality: 80
-    },
-    pngquant: {
-      speed: 3,
-      strip: true,
-      verbose: true
-    },
-    svgo: {
-      plugins: [
-        {
-          name: 'preset-default',
-          params: {
-            overrides: {
-              cleanupIDs: false
-            }
-          }
-        }
-      ]
-    }
-  }]
-], {
+// next.config.js
+module.exports = {
   images: {
-    disableStaticImages: false
+    unoptimized: true
   }
-})
+}
